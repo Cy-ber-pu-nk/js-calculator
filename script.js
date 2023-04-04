b53b6bf7a220c5da6d979071b900151193b6c381
Cy-ber-pu-nk <cpunk6492@gmail.com>
2023-04-04 01:49:42 -0700

    js calculator

---
+++ script.js
@@ -0,0 +1,116 @@
+let input1 =document.getElementById("input1")
+
+
+function one(){
+
+   input1.value += "1"
+}
+
+
+function two(){
+
+    input1.value += "2"
+}
+
+
+function three(){
+
+    input1.value += "3"
+}
+
+
+function four(){
+
+    input1.value += "4"
+}
+
+
+function five(){
+
+    input1.value += "5"
+}
+ 
+
+function six(){
+
+    input1.value += "6"
+}
+
+
+function seven(){
+
+    input1.value += "7"
+}
+
+
+function eight(){
+
+    input1.value += "8"
+}
+
+
+function nine(){
+
+    input1.value += "9"
+}
+
+
+function zero(){
+
+    input1.value += "0"
+}
+
+function add(){
+
+    input1.value += "+"
+}
+
+
+function sub(){
+
+    input1.value += "-"
+}
+
+
+function Dzero(){
+
+    input1.value += "00"
+}
+
+
+function divide(){
+
+    input1.value += "/"
+}
+
+function star(){
+   
+    input1.value += "*"
+}
+
+
+function dot(){
+
+    input1.value += "."
+}
+
+
+function clearD(){
+    document.getElementById("input1").value = " "
+}
+
+
+function dash(){
+    input1.value = eval(document.getElementById("input1").value)
+}
+
+
+function Ac(){
+    let input1 =document.getElementById("input1")
+    input1.value = input1.value.slice(0, -1)
+}
+
+
+
+
+
